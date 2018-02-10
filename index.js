48c127bf66bdc3b1726360f8a61149148ef573e2
Babisha Manandhar <babisha.manandhar@gmail.com>
2018-02-10 23:12:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = { flour: 'white', sugar: 'brown', cream: 'buttercream'}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObj = Object.assign ({},fruit:'mango', recipes)
+  var newObj = Object.assign ({}, recipes)
     
     return newObj
 }
