430555a15710aab49cfc73316c79d48b49f82c24
Babisha Manandhar <babisha.manandhar@gmail.com>
2018-02-10 23:10:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var recipes = { flour: 'white', sugar: 'brown', cream: 'buttercream'}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign ({}, recipes)
+    
+    return newObj
+}
+
